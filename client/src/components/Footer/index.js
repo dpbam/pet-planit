31c5d08ae6d1934594538f58ceb20fd7f357b219
dpbam <derekphtpt@gmail.com>
2021-10-03 17:11:08 -0600

    Footer.js, no styling yet

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+export const Footer = () => {
+  return (
+    <div>
+      <h1>
+        <a
+          href="https://github.com/dpbam'
+          target='_blank'
+          rel='noreferrer noopener"
+        >
+          Derek Phelps
+        </a>
+      </h1>
+      <h1>
+        <a
+          href="https://github.com/emholmes target='_blank'
+          rel='noreferrer noopener"
+        >
+          Erin Holmes
+        </a>
+      </h1>
+      <h1>
+        <a
+          href="https://github.com/freezenleo target='_blank'
+          rel='noreferrer noopener"
+        >
+          Jiaming (Clark) Zhou
+        </a>
+      </h1>
+      <h1>
+        <a
+          href="https://github.com/meghankbock target='_blank'
+          rel='noreferrer noopener"
+        >
+          Meghan Bock
+        </a>
+      </h1>
+      <h1>
+        <a
+          href="https://github.com/TimPrimmer target='_blank'
+          rel='noreferrer noopener"
+        >
+          Tim Primmer
+        </a>
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
