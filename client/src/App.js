abbe94f1286d66f4fe46cc7581c03431e7a7c3d0
Erin Holmes <e.holmes09@yahoo.com>
2021-10-03 19:31:15 -0500

    update package.json to include dependencies, temp text for nav just to see something when starting up react, can now use npm run develop from root

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className=''></div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      {/* temporary */}
+      <p>Hello world!</p>
+    </nav>
+  )
+};
+
+export default Nav;
