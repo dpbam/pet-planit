63807fce3bad85e4c08ab044c112e2f8676c1245
Erin Holmes <e.holmes09@yahoo.com>
2021-10-05 11:39:19 -0500

    add Router and footer component to app.js, import react-icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
+    <Router>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </Router>
     
-    <div>
-      <Header />
-    </div>
   );
 }
 
