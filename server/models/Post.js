45181a480e21ca592eaa386c1e16dbb6393f3d4f
Meghan Bock <meghan.bock@gmail.com>
2021-10-05 08:43:45 -0500

    set up Post model

---
+++ server/models/Post.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+//const replySchema = require('./Reaction');
+//const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema(
+  {
+    postText: {
+      type: String,
+      required: 'You need to leave a post!',
+      minlength: 1,
+      maxlength: 1000,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    replies: [replySchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+postSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
