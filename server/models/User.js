91dda42785fdee795349af3e56843adaa2bab7fb
Meghan Bock <meghan.bock@gmail.com>
2021-10-05 08:31:42 -0500

    set up User model

---
+++ server/models/User.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    pets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Pet'
+      }
+    ],
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('petCount').get(function() {
+  return this.pets.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
