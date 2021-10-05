069249ad667937f0fdda17a07163c46462e96adf
Meghan Bock <meghan.bock@gmail.com>
2021-10-05 08:39:30 -0500

    set up Pet model

---
+++ server/models/Pet.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const petSchema = new Schema(
+  {
+    petName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    petType: {
+      type: String,
+      required: true,
+      // match: enums: dog, cat, ...
+    },
+    dogBreed: {
+      type: String,
+      required: true,
+      // match: enums: 
+    },
+    petAge: {
+        type: Int
+    },
+    about: {
+        type: String,
+        trim: true,
+    },
+    owner: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Pet = model("Pet", petSchema);
+
+module.exports = Pet;
