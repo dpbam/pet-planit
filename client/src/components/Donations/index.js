b0868229675a413df0c19ba2aad932b2a116114f
dpbam <derekphtpt@gmail.com>
2021-10-07 17:58:08 -0600

    buttons on donations page first version

---
+++ client/src/components/Donations/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Donations = () => {
+  // array of charity websites with link just like portfolio projects page
+  return (
+    <div>
+      <button>Wildlife Conservation Society</button>
+      <button>Wildlife Conservation Society</button>
+      <button>Wildlife Conservation Society</button>
+      <button>Wildlife Conservation Society</button>
+      <button>Wildlife Conservation Society</button>
+      <button>Wildlife Conservation Society</button>
+      <button>Wildlife Conservation Society</button>
+    </div>
+  );
+};
