e6f75959e19889489a510ba53ffa1a3e78213f26
dpbam <derekphtpt@gmail.com>
2021-10-07 21:38:56 -0600

    LOGIN_USER in mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
