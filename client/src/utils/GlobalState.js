078317a222e0bc5ed44215770c97f04678d73208
Clark <81452611+freezenleo@users.noreply.github.com>
2021-10-07 19:37:03 -0500

    nav bar

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from "react";
+import { useNewsReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        posts: [],
+        categories: [],
+        currentCategory: '',
+        cart: [],
+        cartOpen: false,
+    });
+
+    //use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
