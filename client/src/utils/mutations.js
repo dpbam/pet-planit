f01c7642903bd9ed64b3886df78d04b16c8039e3
dpbam <derekphtpt@gmail.com>
2021-10-08 11:05:21 -0600

    ADD_FRIEND and REMOVE_FRIEND mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -56,3 +56,30 @@ export const UPDATE_USER = gql`
     }
   }
 `;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_FRIEND = gql`
+  mutation removeFriend($id: ID!) {
+    removeFriend(id: $id) {
+      _id
+      username
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
