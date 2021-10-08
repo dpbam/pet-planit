89556681457674c22b0ee93bbbb2c77196da5a24
Tim Primmer <timprimmer@gmail.com>
2021-10-08 13:58:31 -0500

    worked on edit logic for profile and pets

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import $ from 'jquery';
 
 // TODO add interests
 
@@ -16,6 +17,48 @@ const Profile = (props) => {
   } = props.profile;
 
 
+  const [editingProfile, setEditingProfile] = useState(false);
+  const [editingPet, setEditingPet] = useState(false);
+
+  const editProfile = () => {
+    setEditingProfile(!editingProfile);
+    // TODO add logic for graphql mutation to update profile
+  }
+
+  const editPet = (petnum) => {
+    setEditingPet(!editingPet);
+    let pets = $(document.getElementsByClassName("pet-box"));
+    let petEditButtons = $(document.getElementsByClassName("pet-edit"));
+    let currentPetButton = $(petEditButtons[petnum]);
+
+    let petPlayButtons = $(document.getElementsByClassName("pet-play-button"));
+
+    console.log(petPlayButtons);
+    $.each(petPlayButtons, (index, button) => {
+      console.log($(button).attr('pet'), petnum);
+      if (Number($(button).attr('pet')) === petnum) {
+        $(button).attr('disabled', !$(button).is('[disabled]')); // flips the editability of the radial buttons
+      }
+    });
+
+    let pet = $(pets[petnum]);
+    let textAreas = pet.find('textarea');
+
+    $.each(textAreas, (index, area) => {
+      $(area).attr('readonly', !$(area).is('[readonly]')); // flips the editability of the input fields
+    });
+
+    if (currentPetButton.hasClass("pet-save")) {
+      currentPetButton.removeClass('pet-save');
+      currentPetButton.text('Edit');
+    } else {
+      currentPetButton.addClass('pet-save');
+      currentPetButton.text('Save');
+    }
+
+    // TODO add logic for graphql mutation to update pets
+  }
+
   return (
     <section id="profile-section">
 
@@ -23,34 +66,69 @@ const Profile = (props) => {
       <div id="profile-details">
         <div id="profile-info">
 
-          <div className="profile-details-holder">
-            <div className="profile-detail-container">
-              <label>First name</label>
-              <textarea type="text" name="owner-fname" defaultValue={firstName} readOnly />
-            </div>
-            <div className="profile-detail-container">
-              <label>Last name</label>
-              <textarea type="text" name="owner-lname" defaultValue={lastName} readOnly />
-            </div>
-            <div className="profile-detail-container">
-              <label>Email</label>
-              <textarea type="text" name="owner-email" defaultValue={email} readOnly />
-            </div>
-            <div className="profile-detail-container">
-              <label>Zipcode</label>
-              <textarea type="text" name="owner-zipcode" defaultValue={zipcode} readOnly />
-            </div>
-          </div>
+          {editingProfile
+            ?
+            <>
+              <div className="profile-details-holder">
+                <div className="profile-detail-container">
+                  <label>First name</label>
+                  <textarea type="text" name="owner-fname" defaultValue={firstName} />
+                </div>
+                <div className="profile-detail-container">
+                  <label>Last name</label>
+                  <textarea type="text" name="owner-lname" defaultValue={lastName} />
+                </div>
+                <div className="profile-detail-container">
+                  <label>Email</label>
+                  <textarea type="text" name="owner-email" defaultValue={email} />
+                </div>
+                <div className="profile-detail-container">
+                  <label>Zipcode</label>
+                  <textarea type="text" name="owner-zipcode" defaultValue={zipcode} />
+                </div>
+              </div>
 
 
-          <div className="profile-image-holder">
-            <div className="profile-button-holder">
-              <button type="button" className="profile-edit button">Edit</button>
-            </div>
-            <div className="profile-image-container">
-              <img src={image} alt={"Picture of " + username} />
-            </div>
-          </div>
+              <div className="profile-image-holder">
+                <div className="profile-button-holder">
+                  <button type="button" className="profile-edit profile-save button" onClick={editProfile}>Save</button>
+                </div>
+                <div className="profile-image-container">
+                  <img src={image} alt={"Picture of " + username} />
+                </div>
+              </div>
+            </>
+            :
+            <>
+              <div className="profile-details-holder">
+                <div className="profile-detail-container">
+                  <label>First name</label>
+                  <textarea type="text" name="owner-fname" defaultValue={firstName} readOnly />
+                </div>
+                <div className="profile-detail-container">
+                  <label>Last name</label>
+                  <textarea type="text" name="owner-lname" defaultValue={lastName} readOnly />
+                </div>
+                <div className="profile-detail-container">
+                  <label>Email</label>
+                  <textarea type="text" name="owner-email" defaultValue={email} readOnly />
+                </div>
+                <div className="profile-detail-container">
+                  <label>Zipcode</label>
+                  <textarea type="text" name="owner-zipcode" defaultValue={zipcode} readOnly />
+                </div>
+              </div>
+
+
+              <div className="profile-image-holder">
+                <div className="profile-button-holder">
+                  <button type="button" className="profile-edit button" onClick={editProfile}>Edit</button>
+                </div>
+                <div className="profile-image-container">
+                  <img src={image} alt={"Picture of " + username} />
+                </div>
+              </div>
+            </>}
 
         </div>
 
@@ -72,7 +150,7 @@ const Profile = (props) => {
       <h2>Pets</h2>
       <form id="profile-pets">
         {pets.map((pet, index) => (
-          <div key={index} className="pet-box">
+          <div key={index} className="pet-box" pet={index}>
             <div className="pet-details-1">
 
               <div className="profile-detail-container">
@@ -102,27 +180,13 @@ const Profile = (props) => {
                 <div className="pet-play-wrapper">
                   <label className="pet-playdate-label">Playdates?</label>
 
-                  {pet.playdate
-                    ?
-                    <>
-                      <div className="playdate-buttons">
-                        <input type="radio" name={pet.name + "-playdate"} disabled={true} checked />Yes
-                      </div>
-                      <div className="playdate-buttons">
-                        <input type="radio" name={pet.name + "-playdate"} disabled={true} />No
-                      </div>
-                    </>
-                    :
-                    <>
-                      <div className="playdate-buttons">
-                        <input type="radio" name={pet.name + "-playdate"} disabled={true} />Yes
-                      </div>
+                  <div className="playdate-buttons">
+                    <input type="radio" className="pet-play-button" pet={index} name={pet.name + "-playdate"} defaultChecked={pet.playdate} disabled={true}  />Yes
+                  </div>
+                  <div className="playdate-buttons">
+                    <input type="radio" className="pet-play-button" pet={index} name={pet.name + "-playdate"} defaultChecked={!pet.playdate}  disabled={true}  />No
+                  </div>
 
-                      <div className="playdate-buttons">
-                        <input type="radio" name={pet.name + "-playdate"} disabled={true} checked />No
-                      </div>
-                    </>
-                  }
                 </div>
               </div>
 
@@ -135,7 +199,7 @@ const Profile = (props) => {
 
             <div className="pet-details-3">
               <div className="pet-button-holder">
-                <button type="button" className="pet-edit button">Edit</button>
+                <button type="button" className="pet-edit button" petnum={index} onClick={() => editPet(index)}>Edit</button>
                 <button type="button" className="pet-delete button">X</button>
               </div>
               <div className="pet-image-container">
