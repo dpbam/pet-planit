e16f9a5fc52328bfd248fceb6ee1fd4b72631a8f
dpbam <derekphtpt@gmail.com>
2021-10-08 15:40:24 -0600

    have some questions to complete the queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -92,13 +92,59 @@ export const QUERY_PET = gql`
 // not sure how to do this one
 export const QUERY_PETS = gql`
     query pets($petId: ID!) {
+        pets(petId: $petId) {
+            
+        }
+    }
+`;
+
+// not sure how to do this one
+export const QUERY_FEEDS = gql`
+    query feeds() {
 
     }
 `;
 
+export const QUERY_FEED = gql`
+    query feed($feedName: String!) {
+        feed(feedName: $feedName) {
+
+        }
+    }
+`;
+
+// is this right?
 export const QUERY_POSTS = gql`
   query posts($username: String) {
     posts(username: $username) {
+      _id
+      postText
+      feedName
+      createdAt
+      username
+      replyCount
+      replies {
+        _id
+        replyText
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+// not sure how to do this one
+export const QUERY_POSTS_BY_FEED = gql`
+    query postsByFeed($feedName: String!) {
+        postsByFeed(feedName: $feedName) {
+
+        }
+    }
+`;
+
+export const QUERY_POSTS_BY_USER = gql`
+  query postsByUser($username: String!) {
+    postsByUser(username: $username) {
       _id
       postText
       feedName
