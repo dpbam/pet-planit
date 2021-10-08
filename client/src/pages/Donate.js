d5b14b5e0e9685ac1b6e56a5f65b7f4735a829a8
Erin Holmes <e.holmes09@yahoo.com>
2021-10-07 23:48:40 -0500

    style donations page for desktop

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -5,15 +5,19 @@ const Donate = () => {
   return (
     <main className="content">
       <div className="hero-donate"></div>
-      <h1>Donate</h1>
-      <section class="donate-orgs">
+      <h1 className="donate-h1">Donate</h1>
+      <section class="donate-paws">
         <p>Lend a helping paw, consider donating to one of these wonderful organizations.</p>
-        {foundations.map((foundation) => (
-          <button key={foundation.id} onClick={() => window.open(foundation.url, "_blank")}>{foundation.name}</button>
-        ))}
+        <div>
+          {foundations.map((foundation) => (
+            <button key={foundation.id} onClick={() => window.open(foundation.url, "_blank")}>{foundation.name}</button>
+          ))}
+        </div>
       </section>
-      <hr/>OR<hr/>
-      <section>
+      <div className="donation-divider">
+        <hr />OR<hr />
+      </div>
+      <section className="donate-site">
           <p>Consider donating to keep this website up and running&#42;:</p>
           <small>&#42;50% of proceeds will be donated to an animal rescue non-profit each month.</small>
       </section>
