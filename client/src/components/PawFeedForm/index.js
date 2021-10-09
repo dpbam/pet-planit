35c0c5a9e5ef3cc0c1cfde01d90dfee82e794c88
Clark <81452611+freezenleo@users.noreply.github.com>
2021-10-09 11:07:50 -0500

    finishing paw feed form

--- client/src/components/PawFeedForm/index.js
+++ client/src/components/PawFeedForm/index.js
@@ -32,4 +32,54 @@ const PostForm = () => {
             });
         }
     });
+
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setText(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    }
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            await addPost({
+                variables: { postText }
+            });
+
+            //clear form value
+            setText('');
+            setCharacterCount(0);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <div>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+                Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
+            </p>
+            <form
+                className="flex-row justify-center justify-space-between-md align-stretch"
+                onSubmit={handleFormSubmit}
+            >
+                <textarea
+                    placeholder="Create a Fun Paw Feed"
+                    value={postText}
+
+                    className="form-input col-12 col-md-9"
+                    onChange={handleChange}
+                ></textarea>
+                <button className="btn col-12 col-md-3" type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
 }
+
+export default PostForm;
