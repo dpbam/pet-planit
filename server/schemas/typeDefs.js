762dc2cd5bd5f55e95d92f88c401d69812aa7c5e
Meghan Bock <meghan.bock@gmail.com>
2021-10-09 17:38:54 -0500

    update typeDefs for updatePet

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -78,9 +78,9 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!, firstName: String!, lastName: String!, zipcode: String, image: String): Auth
-        updateUser(username: String, email: String, password: String, firstName: String, lastName: String, zipcode: String, image: String): Auth
+        updateUser(username: String, email: String, password: String, firstName: String, lastName: String, zipcode: String, image: String): User
         addPet(petName: String!, petType: String!, petAge: Int!, petBreed: String, playDate: Boolean, about: String, image: String): Pet
-        updatePet(petId: ID!, petName: String!, petType: String!, petAge: Int!, petBreed: String, playDate: Boolean, about: String, image: String): Pet
+        updatePet(petId: ID!, petName: String, petType: String, petAge: Int, petBreed: String, playDate: Boolean, about: String, image: String): Pet
         deletePet(petId: ID!): Pet
         addPost(postText: String!, feedName: String!): Post
         updatePost(postId: ID!, postText: String!): Post
