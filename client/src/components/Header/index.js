e1db4ad6bec56c3dc0a6ca685c8f60ff38aafee4
Erin Holmes <e.holmes09@yahoo.com>
2021-10-10 13:53:16 -0500

    update header to underline active page in nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import Auth from "../../utils/auth";
 
 const Header = () => {
   const [activeNav, setActiveNav] = useState("signup");
+
+  useEffect(() => {
+    let relativeUrl = window.location.pathname.substring(1);
+    if (relativeUrl === "") {
+      relativeUrl = window.location.hash;
+    }
+    setActiveNav(relativeUrl)
+  }, [activeNav]);
 
   const logout = (event) => {
     event.preventDefault();
@@ -20,15 +28,15 @@ const Header = () => {
       <nav>
         {Auth.loggedIn() ? (
           <ul>
-            <li><Link to="/pawfeed">Pawfeed</Link></li>
-            <li><Link to='/profile'>My Profile</Link></li>
-            <li><Link to="/donate">Donate</Link></li>
+            <li><Link to="/pawfeed" className={activeNav === "pawfeed" ? "active" : ""} onClick={() => setActiveNav("pawfeed")}>Pawfeed</Link></li>
+            <li><Link to="/profile" className={activeNav === "profile" ? "active" : ""} onClick={() => setActiveNav("profile")}>My Profile</Link></li>
+            <li><Link to="/donate" className={activeNav === "donate" ? "active" : ""} onClick={() => setActiveNav("donate")}>Donate</Link></li>
             <li><a href='/' onClick={logout}>Logout</a></li>
           </ul>
         ) : (
           <ul>
-            <li><a href="#about" className={activeNav === "about" ? "active" : ""} onClick={() => setActiveNav("about")}>About</a></li>
-            <li><a href="/#" className={activeNav === "signup" ? "active" : ""} onClick={() => setActiveNav("signup")}>Sign-up / Login</a></li>
+            <li><a href="#about" className={activeNav === "#about" ? "active" : ""} onClick={() => setActiveNav("#about")}>About</a></li>
+            <li><a href="/" className={activeNav === "" ? "active" : ""} onClick={() => setActiveNav("signup")}>Sign-up / Login</a></li>
           </ul>
         )}
       </nav>
